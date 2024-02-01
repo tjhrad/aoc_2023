ed5fa183430095013d8dc3c3416b60a08c6f8235
Trevor Huff <trevh5@gmail.com>
2024-01-31 21:24:55 -0700

    Day 2 part 1 done.

--- 3_1/src/day3_1.cpp
+++ 3_1/src/day3_1.cpp
@@ -8,9 +8,9 @@ int main()
   std::vector<std::string> file_data = read_text_file(file_name);
 
   std::vector<std::vector<char>> file_data_as_chars = strings_to_chars(file_data);
-  std::vector<std::vector<int>> symbols_binary_matrix = chars_to_binary_matrix(file_data_as_chars);
-  std::vector<std::vector<int>> codes_matrix = get_codes_matrix(file_data_as_chars);
-  std::vector<std::vector<std::vector<int>>> separated_code_matrices = split_code_matrices(codes_matrix);
+  std::vector<std::vector<int>> symbols_binary_matrix = get_symbols_binary_matrix(file_data_as_chars);
+  std::vector<std::vector<char>> codes_matrix = get_codes_matrix(file_data_as_chars);
+  std::vector<std::vector<std::vector<char>>> separated_code_matrices = split_code_matrices(codes_matrix);
   std::vector<int> valid_codes = get_valid_codes(separated_code_matrices, symbols_binary_matrix);
 
   for (int code : valid_codes)
@@ -27,11 +27,11 @@ int main()
 
 //TODO: Currently the code does not account for 0s in codes such as -- 101... Refactor the algo to account for this.
 
-std::vector<int> get_valid_codes(std::vector<std::vector<std::vector<int>>> code_matrices, std::vector<std::vector<int>> binary_symbols)
+std::vector<int> get_valid_codes(std::vector<std::vector<std::vector<char>>> code_matrices, std::vector<std::vector<int>> binary_symbols)
 {
   std::vector<int> valid_codes;
   
-  for (std::vector<std::vector<int>> code_matrix : code_matrices)
+  for (std::vector<std::vector<char>> code_matrix : code_matrices)
   {
     std::vector<std::vector<int>> temporary_binary_map;
     std::vector<std::vector<int>> product_matrix;
@@ -70,18 +70,19 @@ std::vector<int> get_valid_codes(std::vector<std::vector<std::vector<int>>> code
   return valid_codes;
 }
 
-int get_code(std::vector<std::vector<int>> code_matrix)
+int get_code(std::vector<std::vector<char>> code_matrix)
 {
   int code;
   std::string code_string = "";
 
-  for (std::vector v : code_matrix)
+  for (std::vector<char> v : code_matrix)
   {
-    for (int i : v)
+    for (char i : v)
     {
-      if (i > 0)
+      if (isdigit(i))
       {
-        code_string += std::to_string(i);
+        code_string += i;
+        //code_string += std::to_string(i);
       }
     }
   }
@@ -91,7 +92,7 @@ int get_code(std::vector<std::vector<int>> code_matrix)
   return code;
 }
 
-std::vector<std::vector<int>> code_matrix_to_binary_map(std::vector<std::vector<int>> code_matrix)
+std::vector<std::vector<int>> code_matrix_to_binary_map(std::vector<std::vector<char>> code_matrix)
 {
   std::vector<std::vector<int>> binary_matrix;
   std::vector<int> stored_y_indices;
@@ -104,7 +105,7 @@ std::vector<std::vector<int>> code_matrix_to_binary_map(std::vector<std::vector<
     std::vector<int> temporary_result;
     for ( int x=0; x<code_matrix[y].size(); x++ )
     {
-      if (code_matrix[y][x] == 0)
+      if (code_matrix[y][x] == '.')
       {
         temporary_result.push_back(0);
       }
@@ -166,9 +167,9 @@ std::vector<std::vector<int>> code_matrix_to_binary_map(std::vector<std::vector<
   return binary_matrix;
 }
 
-std::vector<std::vector<std::vector<int>>> split_code_matrices(std::vector<std::vector<int>> codes_matrix)
+std::vector<std::vector<std::vector<char>>> split_code_matrices(std::vector<std::vector<char>> codes_matrix)
 {
-  std::vector<std::vector<std::vector<int>>> split_matrices;
+  std::vector<std::vector<std::vector<char>>> split_matrices;
   bool is_done = false;
   
   while(!is_done)
@@ -176,86 +177,113 @@ std::vector<std::vector<std::vector<int>>> split_code_matrices(std::vector<std::
     bool is_building_code = false;
     bool has_built_code = false;
 
-    std::vector<std::vector<int>> temporary_matrix;
+    std::vector<std::vector<char>> temporary_matrix;
 
-    for(std::vector<int> v : codes_matrix)
+    //for(std::vector<char> v : codes_matrix)
+    for(int x = 0; x<codes_matrix.size(); x++)
     {
-      std::vector<int> temporary_vector;
-      for(int i : v)
+      std::vector<char> temporary_vector;
+      //for(char i : v)
+      if(is_building_code)
       {
-        if (i != 0)
+        has_built_code = true;
+        is_building_code = false;
+      }
+      for (int i = 0; i<codes_matrix[x].size(); i++)
+      {
+        //if (isdigit(i))
+        if(isdigit(codes_matrix[x][i]))
         {
           if(!is_building_code && !has_built_code)
           {
             is_building_code = true;
-            temporary_vector.push_back(i);
+            temporary_vector.push_back(codes_matrix[x][i]);
+            codes_matrix[x][i] = '.';
           }
           else if (is_building_code)
           {
-            temporary_vector.push_back(i);
+            temporary_vector.push_back(codes_matrix[x][i]);
+            codes_matrix[x][i] = '.';
           }
           else if (has_built_code)
           {
-            temporary_vector.push_back(0);
+            temporary_vector.push_back('.');
           }
         }
-        else if (i == 0)
+        else if (codes_matrix[x][i] == '.')
         {
           if(is_building_code)
           {
             is_building_code = false;
             has_built_code = true;
           }
-          temporary_vector.push_back(i);
+          temporary_vector.push_back(codes_matrix[x][i]);
         }
       }
 
       temporary_matrix.push_back(temporary_vector);
 
     }
-    codes_matrix = subtract_2d_vectors(codes_matrix, temporary_matrix);
+    //codes_matrix = subtract_2d_vectors(codes_matrix, temporary_matrix);
     split_matrices.push_back(temporary_matrix);
 
 
-    int sum = 0;
-    for(std::vector<int> v : codes_matrix)
+    bool has_digit = false;
+    for(int x = 0; x<codes_matrix.size(); x++)
     {
-      for (int i : v)
+      for (int i = 0; i<codes_matrix[x].size(); i++)
       {
-        sum += i;
+        if(isdigit(codes_matrix[x][i]))
+        {
+          has_digit = true;
+        }
       }
     }
 
-    if(sum ==  0)
+
+    if(!has_digit)
     {
       is_done = true;
     }
+    /* int sum = 0; */
+    /* for(std::vector<int> v : codes_matrix) */
+    /* { */
+    /*   for (int i : v) */
+    /*   { */
+    /*     sum += i; */
+    /*   } */
+    /* } */
+    /**/
+    /* if(sum ==  0) */
+    /* { */
+    /*   is_done = true; */
+    /* } */
   }
 
   return split_matrices;
 
 }
 
-std::vector<std::vector<int>> get_codes_matrix(std::vector<std::vector<char>> char_matrix)
+std::vector<std::vector<char>> get_codes_matrix(std::vector<std::vector<char>> char_matrix)
 {
-  std::vector<std::vector<int>> codes_matrix;
+  std::vector<std::vector<char>> codes_matrix;
 
   for (std::vector v : char_matrix)
   {
-    std::vector<int> int_vector;
+    std::vector<char> char_vector;
     for (char c : v)
     {
       if ( isdigit(c) )
       {
-        std::string s(1,c);
-        int_vector.push_back(stoi(s));
+        //std::string s(1,c);
+        char_vector.push_back(c);
       }
       else 
       {
-        int_vector.push_back(0);
+        char_vector.push_back('.');
       }
     }
-  codes_matrix.push_back(int_vector);
+  codes_matrix.push_back(char_vector);
   }
 
   return codes_matrix;
@@ -275,7 +303,7 @@ std::vector<std::vector<char>> strings_to_chars(std::vector<std::string> strings
   return converted_chars;
 }
 
-std::vector<std::vector<int>> chars_to_binary_matrix(std::vector<std::vector<char>> char_matrix)
+std::vector<std::vector<int>> get_symbols_binary_matrix(std::vector<std::vector<char>> char_matrix)
 {
   std::vector<std::vector<int>> binary_matrix;
 
--- 3_1/src/day3_1.h
+++ 3_1/src/day3_1.h
@@ -8,16 +8,16 @@
 
 std::vector<std::vector<char>> strings_to_chars(std::vector<std::string> strings);
 
-std::vector<std::vector<int>> chars_to_binary_matrix(std::vector<std::vector<char>>);
+std::vector<std::vector<int>> get_symbols_binary_matrix(std::vector<std::vector<char>>);
 
-std::vector<std::vector<int>> get_codes_matrix(std::vector<std::vector<char>> char_matrix);
+std::vector<std::vector<char>> get_codes_matrix(std::vector<std::vector<char>> char_matrix);
 
-std::vector<std::vector<std::vector<int>>> split_code_matrices(std::vector<std::vector<int>> codes_matrix);
+std::vector<std::vector<std::vector<char>>> split_code_matrices(std::vector<std::vector<char>> codes_matrix);
 
-std::vector<std::vector<int>> code_matrix_to_binary_map(std::vector<std::vector<int>> code_matrix);
+std::vector<std::vector<int>> code_matrix_to_binary_map(std::vector<std::vector<char>> code_matrix);
 
-std::vector<int> get_valid_codes(std::vector<std::vector<std::vector<int>>> code_matrices, std::vector<std::vector<int>> binary_symbols);
+std::vector<int> get_valid_codes(std::vector<std::vector<std::vector<char>>> code_matrices, std::vector<std::vector<int>> binary_symbols);
 
-int get_code(std::vector<std::vector<int>> code_matrix);
+int get_code(std::vector<std::vector<char>> code_matrix);
 
 #endif 
