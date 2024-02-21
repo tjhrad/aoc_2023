12c83d806262b050af44ae327802e3ceff5fccfd
Trevor Huff <trevh5@gmail.com>
2024-02-20 23:00:54 -0700

    Init 7

---
+++ 7_1/src/day7_1.cpp
@@ -0,0 +1,49 @@
+// Advent of Code 2023 Day 6: Part 2
+
+#include "day7_1.h"
+
+int main()
+{
+  std::string input_file_path = "./data/input.txt";
+
+  std::vector<std::string> input_file_data = read_text_file(input_file_path);
+
+  std::vector<std::string> split_time_string = split_string(input_file_data[0],":");
+  std::vector<std::string> split_distance_string = split_string(input_file_data[1],":");
+
+  long long time = combine_integers_in_string(split_time_string[1]);
+  long long distance = combine_integers_in_string(split_distance_string[1]);
+
+  std::vector<long long> best_possible_distances = get_best_possible_distances(time,distance);
+  
+  auto result = calculate_result(best_possible_distances);
+  
+  std::cout << result << '\n';
+
+  return 0;
+}
+
+int calculate_result(const std::vector<long long>& best_possible_distances)
+{
+  int count = 0;
+  for (long long race : best_possible_distances)
+  {
+    count++;
+  }
+  
+  return count;
+}
+
+std::vector<long long> get_best_possible_distances(const long long& time, const long long& distance)
+{
+  std::vector<long long> best_distances;
+
+  for (long long x = 1; x < time; x++)
+  {
+    long long d = x*(time-x);
+    if (d > distance)
+      best_distances.push_back(d);
+  }
+
+  return best_distances;
+}
---
+++ 7_1/src/day7_1.h
@@ -0,0 +1,12 @@
+#ifndef DAY7_1H
+#define DAY7_1H
+
+#include <iostream>
+
+#include "aoc_tools.h"
+
+std::vector<long long> get_best_possible_distances(const long long& time, const long long& distance);
+
+int calculate_result(const std::vector<long long>& best_possible_distances);
+
+#endif 
