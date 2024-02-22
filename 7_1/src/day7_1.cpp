9c924389fc42fc9e451e2bc0fa6d392b1be49294
Trevor Huff <trevh5@gmail.com>
2024-02-21 23:11:04 -0700

    Day 7 init

--- 7_1/src/day7_1.cpp
+++ 7_1/src/day7_1.cpp
@@ -8,18 +8,6 @@ int main()
 
   std::vector<std::string> input_file_data = read_text_file(input_file_path);
 
-  std::vector<std::string> split_time_string = split_string(input_file_data[0],":");
-  std::vector<std::string> split_distance_string = split_string(input_file_data[1],":");
-
-  long long time = combine_integers_in_string(split_time_string[1]);
-  long long distance = combine_integers_in_string(split_distance_string[1]);
-
-  std::vector<long long> best_possible_distances = get_best_possible_distances(time,distance);
-  
-  auto result = calculate_result(best_possible_distances);
-  
-  std::cout << result << '\n';
-
   return 0;
 }
 
@@ -32,18 +20,4 @@ int calculate_result(const std::vector<long long>& best_possible_distances)
   }
   
   return count;
-}
-
-std::vector<long long> get_best_possible_distances(const long long& time, const long long& distance)
-{
-  std::vector<long long> best_distances;
-
-  for (long long x = 1; x < time; x++)
-  {
-    long long d = x*(time-x);
-    if (d > distance)
-      best_distances.push_back(d);
-  }
-
-  return best_distances;
 }
--- 7_1/src/day7_1.h
+++ 7_1/src/day7_1.h
@@ -5,8 +5,6 @@
 
 #include "aoc_tools.h"
 
-std::vector<long long> get_best_possible_distances(const long long& time, const long long& distance);
-
 int calculate_result(const std::vector<long long>& best_possible_distances);
 
 #endif 
