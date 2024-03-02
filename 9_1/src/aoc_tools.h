58472c2bac32f6d0d3d7f8bbb5da39a4a08644b6
Trevor Huff <trevh5@gmail.com>
2024-03-01 22:24:07 -0700

    Minimal changes day 9

--- 9_1/src/aoc_tools.h
+++ 9_1/src/aoc_tools.h
@@ -21,11 +21,11 @@ long long combine_integers_in_string(const std::string& input_data);
 
 std::vector<std::vector<char>> strings_to_chars(const std::vector<std::string>& strings);
 
-std::vector <std::string> split_string(std::string string_to_split,std::string delimiter);
+std::vector<std::string> split_string(std::string string_to_split,std::string delimiter);
 
 int sum_integers(const std::vector<int>& v);
 
-std::vector <std::string> read_text_file(const std::string& file_name);
+std::vector<std::string> read_text_file(const std::string& file_name);
 
 std::vector<std::vector<int>> subtract_2d_vectors(const std::vector<std::vector<int>>& vector1, const std::vector<std::vector<int>>& vector2);
 
--- 9_1/src/day9_1.cpp
+++ 9_1/src/day9_1.cpp
@@ -8,9 +8,24 @@ int main()
 
   std::vector<std::string> input_file_data = read_text_file(input_file_path);
 
-  long long result = 
+  long result = calculate_result(input_file_data);
 
   std::cout << "Answer: " << result << '\n';
 
   return 0;
+}
+
+
+long calculate_result(const)
+{
+  long result = 0;
+
+  return result;
+}
+
+long get_next_value(const std::vector<long>& input_sequence)
+{
+  long next_value = 0;
+
+  return next_value;
 }
